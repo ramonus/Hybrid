7191f0925109410287517ed28e8f79900599b910
root <root@localhost.localdomain>
2017-11-24 20:57:22 +0100

    Intermediate client server edit

--- client.py
+++ client.py
@@ -9,7 +9,7 @@ Created on Tue Nov 14 15:49:52 2017
 import socket
 from RSA import *
 from AES import *
-
+"""
 info = ("192.168.1.49",5000)
 s = socket.socket()
 s.connect(info)
@@ -29,3 +29,17 @@ print("sending:",enc)
 s.send(enc)
 print("sent")
 s.close()
+"""
+
+class Client(socket.socket):
+  
+  def __init__(self,conf):
+    super().__init__()
+    self.conf = conf
+    self.connect(self.conf)
+    #recive pubk
+    print("Connection established.\nWaiting public key")
+    pubk = self.recv(4096)
+    print("Public key recived validating...")
+
+
