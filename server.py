0f369559aa10b172b6136180fecaf497fedb395c
root <root@localhost.localdomain>
2017-11-24 20:47:27 +0100

    Intermediate client server edit

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ Este es un archivo temporal
 """
 
 
-import socket
+import socket,threading,json
 from AES import *
 from RSA import *
 
@@ -37,4 +37,37 @@ def main():
     conn.close()
     s.close()
     print("End")
-    return rsa,aes,ekey,key,
+    return rsa,aes,ekey,key
+  
+class server(socket.socket):
+  
+  def __init__(self,conf=('',4777)):
+    super().__init__(socket.AF_INET,socket.SOCK_STREAM)
+    self.ip,self.port = self.conf = conf
+    self.running = False
+    self.connections = []
+  def start(self):
+    #listen for connections
+    self.bind(self.conf)
+    self.listen(1)
+    self.running = True
+    while self.running:
+      conn,addr = cinfo = self.accept()
+      self.connections.append(cinfo)
+      th = threading.Thread(target=self.__cHandler,args=cinfo)
+      th.start()
+      self.cThreads.append(th)
+      
+  def stop(self):
+    self.close()
+  def __cHandler(self,conn,addr):
+    print("Handling connection from:",addr[0])
+    #start secure connection
+    
+  def secureConn(self,conn):
+    self.rsa = RSAC()
+    self.rsa.generateKeys()
+    conn.send(self.rsa.publickey.exportKey("PEM").decode())
+    data = json.loads(conn.recv(4096))
+    eaeskey = data["eaeskey"]
+    
