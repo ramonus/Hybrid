0b7b5f19ecc619de56f5124bde38182defe5db5d
ramonus <ralmatob@gmail.com>
2017-11-26 18:43:35 +0100

    Cleaner code

--- client.py
+++ client.py
@@ -9,27 +9,6 @@ Created on Tue Nov 14 15:49:52 2017
 import socket,json,codecs
 from RSA import *
 from AES import *
-"""
-info = ("192.168.1.49",5000)
-s = socket.socket()
-s.connect(info)
-#revice pubkey
-print("listening for public key...")
-pubk = s.recv(4096)
-rsa = RSAC(publickey=pubk)
-# generate and send crypted AES key
-aes = AESC()
-enk = rsa.encrypt(aes.key)
-print("Sent key: {}\nReal Key: {}".format(enk,aes.key))
-s.send(enk)
-
-st = "Hola Ramon Aixo es una prova".encode()
-enc = aes.encrypt(st)
-print("sending:",enc)
-s.send(enc)
-print("sent")
-s.close()
-"""
 
 class Client(socket.socket):
   
--- server.py
+++ server.py
@@ -10,36 +10,6 @@ import socket,threading,json,codecs
 from AES import *
 from RSA import *
 
-"""
-def main():
-    #server
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    
-    port = 5000
-    s.bind(("",port))
-    
-    s.listen(1)
-    print("Listening connections...")
-    conn,addr = s.accept()
-    print("Connection from:",str(addr))
-    # send public key
-    rsa = RSAC()
-    rsa.generateKeys()
-    print("Sending public key")
-    conn.send(rsa.publickey.exportKey("PEM"))
-    print("public key sent")
-    #waiting for his AES key
-    ekey = conn.recv(4096)
-    key = rsa.decrypt(ekey)
-    aes = AESC(key)
-    print("Recived key: {}\nDecrypted key: {}".format(ekey,key))
-    data = conn.recv(4096)
-    print("\nRecived data: {}\nDecrypted data: {}".format(data,aes.decrypt(data)))
-    conn.close()
-    s.close()
-    print("End")
-    return rsa,aes,ekey,key
-"""
 class Server(socket.socket):
   
   def __init__(self,conf=('',4777)):
